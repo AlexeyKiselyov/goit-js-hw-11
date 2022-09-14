c4ebfd29608f1b447b86c3a92da6b9c194d055c7
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-09-14 13:41:11 +0300

    remove coments

--- src/js/galleryMarkup.js
+++ src/js/galleryMarkup.js
@@ -32,34 +32,3 @@ export function galleryMarkup(arr) {
     .join('');
   return newMarkup;
 }
-
-/* <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>; */
-
-//    <div class="photo-card">
-//   <img src="" alt="" loading="lazy"/>
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>;
-
-// hits[{}]
-// webformatURL  - ссылка на маленькое изображение для списка карточек.
-// largeImageURL - ссылка на большое изображение.
-// tags - строка с описанием изображения. Подойдет для атрибута alt.
-// likes - количество лайков.
-// views - количество просмотров.
-// comments - количество комментариев.
-// downloads - количество загрузок.
